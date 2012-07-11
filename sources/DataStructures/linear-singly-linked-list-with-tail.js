b3a94569f47c580d1897ac3f12a6aa5f969c11e1
bp <brian.john.porter@gmail.com>
2012-07-11 17:42:50 -0400

    changed names, added linear singly linked list with a tail (for O(n) insert at end), and modified boundry check, removed count

---
+++ sources/DataStructures/linear-singly-linked-list-with-tail.js
@@ -0,0 +1,107 @@
+function Node() {
+    this.data = null;
+    this.next = null;
+}
+
+function LinkedList() {
+    this.head = null;
+    this.tail = null;
+}
+
+LinkedList.prototype.add = function(data) {    
+    if(this.head === null) {
+        console.log('The list is empty');
+
+        this.head = new Node();
+        this.head.data = data;
+        this.tail = this.head;
+    }
+    else {
+        console.log('The list is not empty');
+        var node = new Node();
+        node.data = data;
+
+        this.tail.next = node;
+        this.tail = node;
+
+        console.log(this.tail.data);
+    }
+    
+    console.log('added: ' + data);
+};
+
+LinkedList.prototype.item = function(index) {
+    //check bounds
+    if(index < 0) throw new OutOfBoundsException();
+    
+    var record = this.head;
+    var count = 0;
+    
+    
+    while(count != index) {
+        if(record.next === null) throw new OutOfBoundsException();
+        else  {
+            record = record.next;
+            count++;    
+        }
+    }
+    
+    console.log('retrieving value ' + record.data + ' at index ' + index);
+    
+    return record.data;
+};
+
+LinkedList.prototype.remove = function(index) {
+     //Check bounds
+     if(index < 0) throw new OutOfBoundsException();
+     
+    var record = this.head; 
+    var count = 0;
+
+    
+    while(count != index) {
+        if(record.next === null) throw new OutOfBoundsException();
+        else  {
+            record = record.next;
+            count++;    
+        }
+    }
+    
+    console.log('removed item ' + record.data + ' at index ' + index + " replaced by " + record.next.data);
+    
+    record.data = record.next.data;
+    record.next = record.next.next;
+    
+    return record.data;
+    
+}
+
+function OutOfBoundsException(message) {
+    this.name = "OutOfBounds";
+    this.message = message || "The index is out of bounds";
+}
+OutOfBoundsException.prototype = new Error();
+OutOfBoundsException.prototype.constructor = OutOfBoundsException;
+
+
+var linkedList = new LinkedList();
+linkedList.add(1);
+linkedList.add(2);
+linkedList.add(3);
+linkedList.add(4);
+
+linkedList.item(0);
+linkedList.item(1);
+linkedList.item(2);
+linkedList.item(3);
+
+linkedList.remove(1);
+
+linkedList.item(0);
+linkedList.item(1);
+linkedList.item(2);
+
+linkedList.remove(1);
+
+linkedList.item(0);
+linkedList.item(1);
