03355e64914e0a9bddc96b0e9dce0130ed0a7f5f
bp <brian.john.porter@gmail.com>
2012-07-16 13:52:06 -0400

    Added binary search tree insert, and size

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Algorithms and Data Structures
 
     Added:
-        1) Linear Singly Linked List
-        2) Linear Singly Linked List with a Tail
-        3) Circular Singly Linked List
-        4) Doubly Linked List
+        1) Linear Singly Linked List (non-recursive)
+        2) Linear Singly Linked List with a Tail (non-recursive)
+        3) Circular Singly Linked List (non-recursive)
+        4) Doubly Linked List (non-recursive)
+        5) [Unfinished] Binary Search Tree (recursive)
---
+++ data-structures/binary-search-tree.js
@@ -0,0 +1,66 @@
+function Node(values) {
+    if(values === undefined) {
+        this.right = null;
+        this.left = null;
+        this.key = null;
+        this.data = null;
+    } else {
+        this.right = values.right === undefined ? null : values.right;
+        this.left = values.left === undefined ? null : values.left;
+        this.key = values.key === undefined ? null : values.key;
+        this.data = values.data === undefined ? null : values.data;
+    }
+}
+
+function BinarySearchTree() {
+    this.root = null;
+}
+
+BinarySearchTree.prototype.insert = function(key, node) {    
+    //Tree is empty, insert the first node
+    if(this.root === null) {
+        this.root = new Node({ key : key });
+        return this.root;
+    } 
+    
+    //node not given, so assume node is root
+    if(this.root !== null && node === undefined)  {
+        node = this.root;   
+    }    
+    
+    if(node.key === null) {
+        node.key = key;
+        return node;
+    }
+    
+    if(key < node.key) {
+        if(node.left === null) {
+            node.left = new Node();
+        }
+        this.insert(key, node.left);
+    }
+    else if(key > node.key) {
+        if(node.right === null) {
+            node.right = new Node();
+        }
+        this.insert(key, node.right);
+    }
+};
+
+BinarySearchTree.prototype.search = function(key, node) {
+    
+};
+
+BinarySearchTree.prototype.size = function(node) {
+    if(this.root === null)
+        return 0;
+    
+    if(this.root !== null && node === undefined)
+        node = this.root;   
+    
+    if(node === null) 
+        return 0;
+    
+    return this.size(node.right) + this.size(node.left) + 1;
+};
+
