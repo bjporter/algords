9f948d4d3c8de5673d7b483c673f2b3c6fd25b0c
bp <brian.john.porter@gmail.com>
2012-07-19 16:20:15 -0400

    Added bozo sort

--- algorithms/sorting/bozo-sort.js
+++ algorithms/sorting/bozo-sort.js
@@ -1,18 +1,31 @@
-function randomNumRange(max, min) {
-    return parseInt(Math.random() * (max - min), 10) + min; 
+/*
+
+Bozo Sort 
+ - Randomly swap 2 elements in the list until the list is in order.
+ - It is more efficient than Bogo Sort
+
+*/
+
+function bozosort(list) {
+    var count = 0;
+    while(!isInOrder(list)) {
+        swap(list, randomNumRange(0, list.length - 1), randomNumRange(0, list.length - 1));
+
+        if(count > 100000000) return;
+    }
 }
 
-function shuffle(list) {
-    for(var i = 0; i < list.length; i++) {
-        swap(list, i, randomNumRange(0, list.length - 1));
-    }
+function randomNumRange(min, max) {
+    return parseInt(Math.random() * ((max + 1) - min), 10) + min; 
 }
 
 function isInOrder(list) {
     var firstVal = list[0];
     for(var i = 1; i < list.length; i++) {
-        if(list[i] < firstVal) return false;
-        else firstVal = list[i];
+        if(list[i] < firstVal) 
+            return false;
+        else 
+            firstVal = list[i];
     }
     
     return true;
@@ -24,17 +37,6 @@ function listCopy(list) {
         newList = list[0];
     }
 }
-
-function bozosort(list) {
-    var count = 0;
-    while(!isInOrder(list)) {
-        shuffle(list);   
-        count++;
-        
-        if(count > 100000000) return;
-    }
-}
-
 
 function swap(list, a, b) {
     var temp = list[a];
