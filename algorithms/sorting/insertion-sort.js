59fb083e36412e077e0c8fa09150eaee764b6d2e
bp <brian.john.porter@gmail.com>
2012-07-21 10:59:27 -0400

    cleaned up insertion sort

--- algorithms/sorting/insertion-sort.js
+++ algorithms/sorting/insertion-sort.js
@@ -1,9 +1,3 @@
-var array_list = [];
-for(var i = 0; i < 10; i++) {
-    array_list[array_list.length] = parseInt(Math.random() * 1000,10);
-}
-
-
 function swap(array_list, a,b) {
     var temp = array_list[b];
     array_list[b] = array_list[a];
@@ -12,19 +6,11 @@ function swap(array_list, a,b) {
 
 
 function insertionsort(array_list) {
-    var timestart = new Date().getTime();
-
     for(var i = 1 ; i < array_list.length; i++) {
         //An element is out of place, move forward it forward in the list in order
         var pos = i;
         
-        while(array_list[pos] < array_list[pos - 1] && pos > 0) {
-            var timeend = new Date().getTime();
-            
-            if(timeend - timestart > 60000) { //If it takes longer than a minute, time out
-                console.log('timed out...');
-                return;
-            }
+        while(array_list[pos] < array_list[pos - 1] && pos > 0) {            
             swap(array_list, pos, pos-1);
             pos--;
         }
